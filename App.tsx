6477f947ed8c7417ec657a8d5d3fe20f8de0d7b1
Matt Weidner <mweidner44@gmail.com>
2019-09-03 17:14:15 +0900

    Wrapped app in Provider and imported Profile View

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Profile from "./views/Profile"
+import { Provider } from 'react-redux'
+import { store } from './src/store'
 
 export default function App() {
+  console.log(store)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Profile/>
+      </View>
+    </Provider>
   )
 }
 
