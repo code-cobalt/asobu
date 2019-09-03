c791b14a4eb3fe49d294cfb262f22672f6b3a02e
arcaster42 <tj.epperson070@gmail.com>
2019-09-04 08:26:12 +0900

    added parser

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 const schema = require("./server/schema")
 const root = require("./server/root")
 
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.once('open', () => console.log('Connected to DB'))
 
