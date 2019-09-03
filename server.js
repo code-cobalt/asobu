3d3224d0defa9119951e51be2591a684746ef612
arcaster42 <tj.epperson070@gmail.com>
2019-09-03 16:48:56 +0900

    implemented server, graphql

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const app = express()
+const port = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const schema = require("./server/schema")
+const root = require("./server/root")
+
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+db.once('open', () => console.log('Connected to DB'))
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        rootValue: root,
+        graphiql: true
+    })
+ )
+
+app.get('/api/test', (req, res) => {
+
+    // res.sendStatus(200)
+})
+
+app.listen(port, () => console.log(`Listening on ${port}`))
