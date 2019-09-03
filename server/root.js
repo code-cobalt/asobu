62bc0dc2d0e6611987e189f3bea9442fc50e5321
arcaster42 <tj.epperson070@gmail.com>
2019-09-03 17:55:04 +0900

    started root

--- server/root.js
+++ server/root.js
@@ -1,14 +1,28 @@
+const mongoose = require('mongoose')
+const [User, Event, Message] = [require('./models/user'), require('./models/event'), require('./models/message')]
+
+
+
 const root = {
     test: () => {
         return 'Test Resolver'
     },
 
-    Users: async () => {
-        return 'Users'
+    Users: () => {
+        User.find({}, (err, val) => {
+             console.log(val)
+        })
+        // User.find().then((results) => {
+        //     return results
+        // })
+        // console.log(User.collection)
+        // console.log(result)
+        // return result
     },
 
-    User: async (email) => {
-        return 'User'
+    User: (email) => {
+        const result = User.find({ email })
+        return result
     }
 
 
