6265dfaba37cce4e61b0d5f3f07d347f02c3343d
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-04 12:16:05 +0900

    seeded db

--- server.js
+++ server.js
@@ -6,10 +6,25 @@ const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
 const schema = require('./server/schema')
 const root = require('./server/root')
+const { Seeder } = require('mongo-seeding')
+const path = require('path')
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.once('open', () => console.log('Connected to DB'))
+
+const config = {
+  database: process.env.DB_URL,
+  dropDatabase: false
+}
+//not allowed to dropDatabase
+const seeder = new Seeder(config)
+const collections = seeder.readCollectionsFromPath(path.resolve('./data'))
+
+seeder
+  .import(collections)
+  .then(() => console.log('Successfully seeded database'))
+  .catch(err => console.log('Error seeding database', err))
 
 app.use(
   '/graphql',
