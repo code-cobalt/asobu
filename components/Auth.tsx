d6f00c42753c23aa63a0a99192366d7cc134590e
Arcaster42 <tj.epperson070@gmail.com>
2019-09-06 08:42:57 +0900

    Created Auth component and included in Wrapper

---
+++ components/Auth.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import { connect } from "react-redux"
+import Login from "../views/Login"
+import Signup from "../views/Signup"
+
+interface Props {
+  showLogin: boolean
+}
+
+class Auth extends Component<Props> {
+  render() {
+    return (
+      <>
+        {this.props.showLogin ? <Login /> : <Signup />}
+      </>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    showLogin: state.showLogin
+  }
+}
+
+export default connect(mapStateToProps, null)(Auth)
