f0d8ba3253570fe61b34c62479fa4f2a0056b14e
Arcaster42 <tj.epperson070@gmail.com>
2019-09-07 22:17:58 +0900

    New components to show list of active chats

---
+++ components/Chat.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+const Chat = props => {
+  //Create markup for the individual chat in the chatlist
+  return (
+    <View>
+      <Text> This is the chat Component </Text>
+    </View>
+  )
+}
+
+export default Chat
