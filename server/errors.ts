b11437fb3cff4bb957ceff66d8e73fd4d23996da
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-08 23:00:57 +0900

    error handling example comment

--- server/errors.ts
+++ server/errors.ts
@@ -1,4 +1,30 @@
 import { createError } from 'apollo-errors'
+import User from '../components/User'
+
+//example of what an invalid graphql request will return:
+
+// bad query example...
+// query {
+//     User(userEmail: "thisemaildoesnotexist") {
+//         first_name
+//         lastname
+//     }
+// }
+//
+//this would return...
+// {
+//     "errors": [
+//       {
+//         "message": "No user account associated with that email address found.",
+//         "name": "InvalidEmailError",
+//         "time_thrown": "2019-09-08T13:56:53.787Z",
+//         "data": {any specified error data goes here...}
+//       }
+//     ],
+//     "data": {
+//       "User": null
+//     }
+//   }
 
 const errors = {
   InvalidEmailError: createError('InvalidEmailError', {
