6b95e600cf3c4bad3dc3c564c763b6bae06d54bf
arcaster42 <tj.epperson070@gmail.com>
2019-09-10 10:59:41 +0900

    added socket logic

--- views/Application.tsx
+++ views/Application.tsx
@@ -1,13 +1,60 @@
 import React from 'react'
 import Main from "./Main"
 import Navbar from "../components/Navbar"
+import { connect } from 'react-redux'
+import localhost from '../environment.js'
+import * as WebSocket from 'ws'
 
-const Application = () => {
-    return (
-        <>
-            <Main />
-            <Navbar />
-        </>)
+interface Socket {
+    ws: WebSocket,
+    open: boolean,
+    connected: boolean
 }
 
-export default Application
+class Application extends React.Component<{}, Socket> {
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: false,
+            connected: false,
+            ws: new WebSocket(localhost)
+        }
+        this.state.ws.onopen = () => {
+            this.setState({ connected: true })
+            console.log('Client Socket Connected')
+        }
+        this.emit = this.emit.bind(this)
+    }
+    componentDidMount() {
+        this.state.ws.OPEN
+    }
+
+    emit() {
+        this.setState(prevState => {
+            open: true
+        })
+        if (this.state.connected) {
+            this.state.ws.send('Message from client')
+        }
+        
+    }
+    render() {
+        return (
+            <>
+                <Main />
+                <Navbar />
+            </>)
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        setSocket: ws => {
+            dispatch({type: 'SET_SOCKET',
+            ws
+            })
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Application)
