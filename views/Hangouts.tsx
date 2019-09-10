c0eac7173751518763e752b1e377942482ba8818
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-10 21:54:53 +0900

    acceptRequest dispatch

--- views/Hangouts.tsx
+++ views/Hangouts.tsx
@@ -1,20 +1,49 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet, TextInput } from "react-native"
-import UserList from "../components/UserList"
+import { View, Text, Image, StyleSheet, TextInput } from 'react-native'
+import UserList from '../components/UserList'
+import { connect } from 'react-redux'
 
-const Hangouts = props => {
+interface UserLimited {
+  first_name: string
+  email: string
+  profile_photo: string
+}
+
+interface Props {
+  receivedHangoutRequests: Array<UserLimited>
+  approveRequest: Function
+}
+
+class Hangouts extends React.Component<Props> {
+  render() {
     return (
-        <View style={styles.userList}>
-            <UserList />
-        </View>
+      <View style={styles.userList}>
+        <UserList />
+      </View>
     )
+  }
 }
 
 const styles = StyleSheet.create({
-    userList: {
-        top: 40,
-    }
+  userList: {
+    top: 40
+  }
 })
 
-export default Hangouts
+const mapStateToProps = state => {
+  return {
+    receivedHangoutRequests: state.receivedHangoutRequests
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    approveRequest: () => {
+      dispatch({ type: 'ACCEPT_REQUEST' })
+    }
+  }
+}
 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Hangouts)
