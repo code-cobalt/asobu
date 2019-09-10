1b62530fb6159bc5d609d385cc40cb06335ae567
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-11 01:04:13 +0900

    modal with received requests

--- views/Hangouts.tsx
+++ views/Hangouts.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet, TextInput } from 'react-native'
+import { View, Text, Image, StyleSheet, Button } from 'react-native'
 import UserList from '../components/UserList'
 import { connect } from 'react-redux'
+import Modal from 'react-native-modal'
+import { postHangoutAccept } from '../src/actions/userActions'
 
 interface UserLimited {
   first_name: string
@@ -11,14 +13,60 @@ interface UserLimited {
 
 interface Props {
   receivedHangoutRequests: Array<UserLimited>
-  approveRequest: Function
+  acceptRequest: Function
+  currentUserEmail: string
 }
 
 class Hangouts extends React.Component<Props> {
+  state = {
+    modalVisible: this.props.receivedHangoutRequests.length > 0
+  }
+  handlePress = async fromEmail => {
+    const res = await postHangoutAccept(this.props.currentUserEmail, fromEmail)
+    if (res.status === 200) {
+      this.props.acceptRequest(fromEmail, res.data.data.AcceptHangoutRequest)
+    }
+  }
   render() {
     return (
       <View style={styles.userList}>
         <UserList />
+        <Modal
+          isVisible={this.state.modalVisible}
+          animationIn="slideInUp"
+          animationOut="slideOutDown"
+          backdropOpacity={0.85}
+          style={styles.modal}
+        >
+          <View style={{ flex: 1 }}>
+            <Text style={styles.title}>
+              Other users would like to hangout with you!
+            </Text>
+            {this.props.receivedHangoutRequests.map(request => {
+              return (
+                <View style={styles.request}>
+                  <Image
+                    source={{ uri: request.profile_photo }}
+                    style={styles.user__image}
+                  />
+                  <View style={{ flexDirection: 'column' }}>
+                    <Text style={styles.user__name}>{request.first_name}</Text>
+                    <Button
+                      onPress={() => this.handlePress(request.email)}
+                      title="Approve Request"
+                    />
+                  </View>
+                </View>
+              )
+            })}
+            <View style={styles.close}>
+              <Button
+                title="Close"
+                onPress={() => this.setState({ modalVisible: false })}
+              />
+            </View>
+          </View>
+        </Modal>
       </View>
     )
   }
@@ -27,18 +75,49 @@ class Hangouts extends React.Component<Props> {
 const styles = StyleSheet.create({
   userList: {
     top: 40
+  },
+  modal: {
+    height: '50%',
+    textAlign: 'center'
+  },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10
+  },
+  request: {
+    flexDirection: 'row',
+    margin: 10
+  },
+  user__image: {
+    borderRadius: 50,
+    height: 90,
+    width: 90
+  },
+  user__name: {
+    color: 'white',
+    fontSize: 40,
+    marginLeft: 10
+  },
+  close: {
+    textAlign: 'right',
+    right: 0,
+    position: 'absolute',
+    bottom: 0
   }
 })
 
 const mapStateToProps = state => {
   return {
-    receivedHangoutRequests: state.receivedHangoutRequests
+    receivedHangoutRequests: state.receivedHangoutRequests,
+    currentUserEmail: state.user.email
   }
 }
 const mapDispatchToProps = dispatch => {
   return {
-    approveRequest: () => {
-      dispatch({ type: 'ACCEPT_REQUEST' })
+    acceptRequest: (fromUserEmail, newChat) => {
+      dispatch({ type: 'ACCEPT_REQUEST', fromUserEmail, newChat })
     }
   }
 }
