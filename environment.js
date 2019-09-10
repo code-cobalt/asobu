a683b0ac632c0b2f342b218ec80a8a610f702304
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-10 10:29:43 +0900

    logging in queries graphql and sets user in store

--- environment.js
+++
@@ -1,9 +0,0 @@
-//replace with your express server IP Address + :3000
-const localhost = 'http://192.168.10.110:3000'
-
-const getApiUrl = () => {
-  if (__DEV__) {
-    return localhost
-  }
-}
-export default getApiUrl
