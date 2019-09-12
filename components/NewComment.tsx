edd03d79e07e4287ea0af3ec563c91d91bebbbf3
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-12 19:16:46 +0900

    createComment

--- components/NewComment.tsx
+++ components/NewComment.tsx
@@ -1,25 +1,69 @@
 import React from 'react'
-import { View, Text, TextInput } from 'react-native'
+import { View, Button, TextInput } from 'react-native'
 import { connect } from 'react-redux'
 import { createComment } from '../src/actions/events'
 
-class NewComment extends React.Component {
+interface Props {
+  createComment: Function
+  eventId: String
+  from: UserLimited
+}
+
+interface UserLimited {
+  first_name: string
+  email: string
+  profile_photo: string
+}
+
+interface State {
+  content: string
+}
+class NewComment extends React.Component<Props, State> {
+  state = {
+    content: ''
+  }
+
   render() {
     return (
       <View>
-        <TextInput>NewComment</TextInput>
+        <TextInput
+          placeholder={'Post new comment...'}
+          onChangeText={text => this.setState({ content: text })}
+        />
+
+        <Button
+          title="Post"
+          onPress={() =>
+            this.props.createComment(this.props.eventId, {
+              from: this.props.from,
+              content: this.state.content
+            })
+          }
+        />
       </View>
     )
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    eventId: state.currentEvent.id,
+    from: {
+      first_name: state.user.first_name,
+      email: state.user.email,
+      profile_photo: state.user.profile_photo
+    }
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
-    createComment: () => dispatch(createComment)
+    createComment: (eventId, newComment) =>
+      dispatch(createComment(eventId, newComment))
   }
 }
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(NewComment)
