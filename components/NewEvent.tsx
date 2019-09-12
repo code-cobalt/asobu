c46f0ad27a4d16c20d4ab30ceb74e6dc5ec18647
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-13 01:16:31 +0900

    new event form

--- components/NewEvent.tsx
+++ components/NewEvent.tsx
@@ -1,76 +1,193 @@
 import React from 'react'
-import { StyleSheet, View, Text, TextInput, ScrollView } from 'react-native'
+import {
+  StyleSheet,
+  Button,
+  View,
+  Text,
+  TextInput,
+  ScrollView
+} from 'react-native'
+import ModalDropdown from 'react-native-modal-dropdown'
+import DateTimePicker from 'react-native-modal-datetime-picker'
 import { connect } from 'react-redux'
 import { createEvent } from '../src/actions/events'
+import Modal from 'react-native-modal'
 
-interface Props {
-  dispatchEvent: Function
+interface UserLimited {
+  first_name: string
+  email: string
+  profile_photo: string
 }
 
-interface State {
+interface Props {
+  visible: boolean
+  createEvent: Function
+  currentUserLimited: UserLimited
+}
+interface Event {
   name: string
   location: string
   description: string
-  // cover_photo: string
-  // limit: number
-  // start: Date
-  // end: Date
-  // tags: Array<string>
+  cover_photo: string
+  limit: number | null
+  start: Date | null
+  end: Date | null
+  tags: string[]
+}
+interface State {
+  newEvent: Event
+  tagsString: string
+  showStartDate: boolean
+  showEndDate: boolean
 }
 
 class NewEvent extends React.Component<Props, State> {
   state = {
-    name: '',
-    location: '',
-    description: ''
-  }
-  handleSubmit = async e => {
-    e.preventDefault()
-    const res = await createEvent(this.state)
-    if (res.status === 200) {
-      this.props.dispatchEvent(res.data.data.CreateEvent)
+    showStartDate: false,
+    showEndDate: false,
+    tagsString: '',
+    newEvent: {
+      name: '',
+      location: '',
+      description: '',
+      cover_photo: '',
+      limit: 10,
+      start: null,
+      end: null,
+      tags: [],
+      creator: this.props.currentUserLimited
     }
   }
 
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.newEvent}>
-        <View style={styles.text__formgroup}>
-          <Text style={styles.input__text}>Name your Event!</Text>
-          <TextInput
-            style={styles.event__input}
-            value={this.state.name}
-            onChangeText={text => this.setState({ name: text })}
-          />
-          <Text style={styles.input__text}>Set a Location!</Text>
-          <TextInput
-            style={styles.event__input}
-            value={this.state.location}
-            onChangeText={text => this.setState({ location: text })}
-          />
-          <Text style={styles.input__text}>Tell us about it!</Text>
-          <TextInput
-            style={styles.event__input}
-            value={this.state.description}
-            onChangeText={text => this.setState({ description: text })}
-          />
-          <Text>Attendee Limit</Text>
-          <Text>Start</Text>
-          <Text>End</Text>
-          <Text>Tags</Text>
-        </View>
-        <View>
-          <Text>Upload a cover photo for your Event!</Text>
-        </View>
-        <View>
-          <Text>Set a start and end time for your event.</Text>
-        </View>
-      </ScrollView>
+      <Modal
+        isVisible={this.props.visible}
+        animationIn="slideInUp"
+        animationOut="slideOutDown"
+        backdropOpacity={1}
+        backdropColor="white"
+      >
+        <ScrollView contentContainerStyle={styles.newEvent}>
+          <View style={styles.text__formgroup}>
+            <Text style={styles.input__text}>Name your Event!</Text>
+            <TextInput
+              style={styles.event__input}
+              onChangeText={text =>
+                this.setState({
+                  newEvent: { ...this.state.newEvent, name: text }
+                })
+              }
+            />
+            <Text style={styles.input__text}>Set a Location!</Text>
+            <TextInput
+              style={styles.event__input}
+              onChangeText={text =>
+                this.setState({
+                  newEvent: { ...this.state.newEvent, location: text }
+                })
+              }
+            />
+            <Text style={styles.input__text}>Tell us about it!</Text>
+            <TextInput
+              style={styles.event__input}
+              onChangeText={text =>
+                this.setState({
+                  newEvent: { ...this.state.newEvent, description: text }
+                })
+              }
+            />
+            <Text>Attendee Limit</Text>
+            <ModalDropdown
+              options={Array.from(Array(101).keys()).slice(1)}
+              onSelect={selection =>
+                this.setState({
+                  newEvent: { ...this.state.newEvent, limit: selection }
+                })
+              }
+            />
+            <Button
+              title="Select Start"
+              onPress={() => this.setState({ showStartDate: true })}
+            />
+            <DateTimePicker
+              isVisible={this.state.showStartDate}
+              mode="datetime"
+              minimumDate={new Date()}
+              onConfirm={date =>
+                this.setState({
+                  newEvent: { ...this.state.newEvent, start: date }
+                })
+              }
+              onCancel={() => this.setState({ showStartDate: false })}
+            />
+            <Button
+              title="Select End"
+              onPress={() => this.setState({ showEndDate: true })}
+            />
+            <DateTimePicker
+              isVisible={this.state.showEndDate}
+              mode="datetime"
+              minimumDate={new Date()}
+              onConfirm={date =>
+                this.setState({
+                  newEvent: { ...this.state.newEvent, end: date }
+                })
+              }
+              onCancel={() => this.setState({ showEndDate: false })}
+            />
+            <Text>Cover Photo</Text>
+            <Text>Tags: {this.state.tagsString}</Text>
+            <ModalDropdown
+              options={[
+                'music',
+                'sports',
+                'art',
+                'food',
+                'social',
+                'pet-friendly',
+                'kid-friendly',
+                'alcohol',
+                'language',
+                'education',
+                'tech',
+                'dance',
+                'books'
+              ]}
+              onSelect={(index, value) => {
+                this.state.newEvent.tags.length === 0
+                  ? this.setState({
+                      tagsString: this.state.tagsString.concat(value)
+                    })
+                  : this.setState({
+                      tagsString: this.state.tagsString.concat(', ' + value)
+                    })
+                this.setState({
+                  newEvent: {
+                    ...this.state.newEvent,
+                    tags: [...this.state.newEvent.tags, value]
+                  }
+                })
+              }}
+            />
+            <Button
+              title="Submit"
+              onPress={() => this.props.createEvent(this.state)}
+            />
+          </View>
+        </ScrollView>
+      </Modal>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  picker: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
   text__formgroup: {
     width: '90%'
   },
@@ -93,15 +210,23 @@ const styles = StyleSheet.create({
   }
 })
 
+const mapStateToProps = state => {
+  return {
+    visible: state.showNewEventForm,
+    currentUserLimited: {
+      first_name: state.user.first_name,
+      email: state.user.email,
+      profile_photo: state.user.profile_photo
+    }
+  }
+}
 const mapDispatchToProps = dispatch => {
   return {
-    dispatchEvent: newEvent => {
-      return dispatch({ type: 'CREATE_EVENT', newEvent })
-    }
+    createEvent: newEvent => dispatch(createEvent(newEvent))
   }
 }
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(NewEvent)
