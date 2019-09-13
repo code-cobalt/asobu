c7297d4ab0a9c563d168a6762487ce7ec149cf69
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-13 13:50:54 +0900

    login query

---
+++ src/queries/users.ts
@@ -0,0 +1,40 @@
+import gql from 'graphql-tag'
+
+export const loginQuery = gql`
+  query Login($userEmail: String!, $userPassword: String!) {
+    Login(userEmail: $userEmail, userPassword: $userPassword) {
+      id
+      first_name
+      last_name
+      email
+      phone_number
+      profile_photo
+      interests
+      exp
+      lvl
+      events {
+        event_id
+        is_creator
+      }
+      chats {
+        chat_id
+        participants {
+          first_name
+          profile_photo
+          email
+        }
+      }
+      sent_hangout_requests {
+        first_name
+        email
+        profile_photo
+      }
+      received_hangout_requests {
+        first_name
+        email
+        profile_photo
+      }
+      imei
+    }
+  }
+`
