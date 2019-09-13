544f66974eb6209aae7b01a0b547b8fbe30dc365
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-13 13:49:58 +0900

    get chat and create message

---
+++ src/actions/chats.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { apiUrl } from '../../environment.js'
+import { print } from 'graphql'
+import { getChatQuery, createMessageQuery } from '../queries/chats'
+
+export const getChat = chatId => {
+  return async dispatch => {
+    const res = await axios.post(`${apiUrl}/graphql`, {
+      query: print(getChatQuery),
+      variables: { chatId }
+    })
+    let chatMessages = res.data.data.Chat
+    chatMessages.length > 0
+      ? (chatMessages = chatMessages.pop().messages)
+      : (chatMessages = [])
+    dispatch({
+      type: 'SHOW_CHAT',
+      messages: chatMessages,
+      chatId
+    })
+  }
+}
+
+export const createMessage = message => {
+  return async dispatch => {
+    const res = await axios.post(`${apiUrl}/graphql`, {
+      query: print(createMessageQuery),
+      variables: { message }
+    })
+    dispatch({ type: 'CREATE_MESSAGE', message: res.data.data.CreateMessage })
+  }
+}
