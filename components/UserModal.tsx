c5e0c677428545078f23b5130088723a5c152095
Matthew Weidner <matthew_weidner@Matthews-MacBook-Pro.local>
2019-09-14 13:00:23 +0900

    UserModal styling and structural changes

--- components/UserModal.tsx
+++ components/UserModal.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import { Text, View, Image, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import { Text, View, Image, TouchableOpacity, TextInput, StyleSheet, ScrollView } from 'react-native'
 import Modal from 'react-native-modal'
 import { connect } from 'react-redux'
 import Badges from "./Badges"
+import { Divider } from 'react-native-elements'
+
 
 interface Props {
   showProfile: boolean
@@ -14,6 +16,7 @@ interface CurrentProfile {
   profile_photo: string,
   lvl: string,
   first_name: string
+  interests: [string]
 }
 
 const UserModal: React.FunctionComponent<Props> = (props) => {
@@ -25,37 +28,44 @@ const UserModal: React.FunctionComponent<Props> = (props) => {
         animationOut="slideOutDown"
         backdropOpacity={1}
         style={styles.modal}
-        backdropColor="#fff"
+        backdropColor="#e5e6e5"
       >
-        <View style={{ flex: 1 }}>
-          <View>
-            <Text>Level {props.currentProfile.lvl}</Text>
-            <Image
-              source={{ uri: props.currentProfile.profile_photo }}
-              style={styles.profile__photo}
-            />
-          </View>
-          <View>
-            <Text>{props.currentProfile.first_name}</Text>
-          </View>
-          <View>
-            <Text>Equipped Badges</Text>
-            <View style={styles.profile__badges}>
-              <Badges />
+        <ScrollView>
+          <View style={styles.profile__header}>
+            <TouchableOpacity style={styles.photo_container}>
+              {props.currentProfile.profile_photo !== null && (
+                <Image
+                  source={{ uri: props.currentProfile.profile_photo }}
+                  style={styles.user__image}
+                />
+              )}
+              {props.currentProfile.profile_photo === null && (
+                <Image
+                  source={require("../assets/default_profile.png")}
+                  style={styles.user__image}
+                />
+              )}
+            </TouchableOpacity>
+            <View style={styles.basic_info_container}>
+              <Text style={styles.user__name}>
+                {props.currentProfile.first_name}
+              </Text>
+              <Text style={styles.user__level}>Level {props.currentProfile.lvl}</Text>
+              <View style={styles.top__badges}>
+                <Badges />
+              </View>
             </View>
           </View>
-          <View>
-            <Text>Hobbies</Text>
-            <TextInput value={'My hobbies are bla bla bla bla'} />
-          </View>
-          <View>
-            <Text>Interests</Text>
-            <TextInput value={'My interest are bla bla bla bla'} />
-          </View>
-          <View>
-            <Text>All Badges</Text>
-            <View style={styles.profile__badges}>
-              <Badges />
+          <Divider style={styles.divider} />
+          <View style={styles.profile__body}>
+            <Text style={styles.info__title}>{props.currentProfile.first_name}'s Interests</Text>
+           
+            <Divider style={styles.divider} />
+            <View style={styles.all__badges}>
+              <Text style={styles.info__title}>All Badges</Text>
+              <View style={styles.badge__container}>
+                <Badges />
+              </View>
             </View>
           </View>
           <TouchableOpacity
@@ -64,7 +74,7 @@ const UserModal: React.FunctionComponent<Props> = (props) => {
           >
             <Text>Close</Text>
           </TouchableOpacity>
-        </View>
+        </ScrollView>
       </Modal>
     </>
   )
@@ -73,14 +83,106 @@ const UserModal: React.FunctionComponent<Props> = (props) => {
 
 const styles = StyleSheet.create({
   modal: {
-    height: '50%',
-    textAlign: 'center'
+  
+  },
+  button__text: {
+    fontWeight: '800',
+    color: 'white'
+  },
+  profile__header: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: "#e5e6e5"
+  },
+  profile__body: {
+    padding: 25,
+    flexDirection: 'column',
+    backgroundColor: "#e5e6e5"
+  },
+  photo_container: {
+    marginTop: 20,
+    height: 255,
+    width: 255,
+    borderRadius: 130,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  basic_info_container: {
+    marginLeft: 10,
+    alignItems: 'center',
+    padding: 25
+  },
+  user__image: {
+    borderRadius: 125,
+    height: 250,
+    width: 250,
+  },
+  user__name: {
+    fontSize: 28,
+    fontWeight: "800"
+  },
+  user__level: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 10
+  },
+  user__xp: {
+    fontSize: 16,
+    fontWeight: '500',
+    marginTop: 5
+  },
+  info__title: {
+    fontSize: 20,
+    alignSelf: 'flex-start',
+    fontWeight: '800',
+  },
+  interests__container: {
+    width: "100%",
+    height: 80,
+    backgroundColor: 'white',
+    marginTop: 15,
+    borderRadius: 10
+  },
+  interests__text: {},
+  email__phone: {
+    marginTop: 25,
+    marginBottom: 25,
+    left: 15
+  },
+  inner__text: {
+    margin: 10,
+    fontSize: 16,
+    fontWeight: '500'
+  },
+  divider: {
+    backgroundColor: '#acbdac',
+    height: 1.5
+  },
+  top__badges: {
+    flexDirection: 'row',
+    padding: 10,
+    marginTop: 5
+  },
+  all__badges: {
+    flexDirection: 'column',
+    marginTop: 25
+  },
+  badge__container: {
+    flexDirection: 'row',
+    marginTop: 25
+  },
+  button__container: {
+    flexDirection: "row",
+    marginTop: 15,
+    marginBottom: 25
   },
   profile__photo: {
     height: 50,
     width: 50
   },
   profile__close: {
+    alignItems: 'center',
+    alignSelf: 'center',
     width: '50%',
     backgroundColor: '#73d961',
     padding: 15,
