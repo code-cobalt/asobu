2c8a4ae31bdfd6829a848e3f868b0fddf7bb7233
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-14 17:17:29 +0900

    clarify comment

--- server/models/hangout.ts
+++ server/models/hangout.ts
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 //participants will be array of email strings
-//status will be pending upon accepted request, ongoing upon meeting, and complete upon finishing
+//status will be 'pending' upon accepted request, 'ongoing' upon meeting, and 'complete' upon finishing
 const HangoutSchema = new mongoose.Schema({
   participants: [String],
   status: String
