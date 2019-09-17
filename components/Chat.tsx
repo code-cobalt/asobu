95e7ec2acd29fbfb2fd5ebb15c4b3b1a7f2fe92a
Matt Weidner <mweidner44@gmail.com>
2019-09-17 23:55:55 +0900

    Working on chats styling, working with width

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -79,7 +79,7 @@ class Chat extends React.Component<Props> {
           {this.props.chat.participants.map(participant => {
             return (
               <View key={participant.email}>
-                <Text style={styles.chat__text}>{participant.first_name}</Text>
+                <Text style={styles.chat__name}>{participant.first_name}</Text>
                 <Text
                   style={styles.chat__text}
                   onPress={() => this.confirmBlock(participant)}
@@ -97,18 +97,25 @@ class Chat extends React.Component<Props> {
 
 const styles = StyleSheet.create({
   chat: {
+    alignSelf: 'center',
+    height: 120,
+    width: '95%',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: 10,
     marginTop: 30,
-    backgroundColor: 'black',
-    borderRadius: 400
+    borderRadius: 40,
+    backgroundColor: '#22222280',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    textShadowRadius: 2,
+    shadowColor: "#000"
   },
   chat__image: {
-    borderRadius: 50,
-    height: 90,
-    width: 90
+    borderRadius: 40,
+    height: '100%',
+    aspectRatio: 2/2
   },
   chat__textcontainer: {
     flexDirection: 'column',
@@ -117,8 +124,15 @@ const styles = StyleSheet.create({
     right: 40
   },
   chat__text: {
+    fontWeight: '700',
     fontSize: 18,
     color: 'white'
+  },
+  chat__name: {
+    fontSize: 28,
+    color: 'white',
+    fontWeight: '900',
+    marginBottom: 30
   },
   chat__badges: {
     flexDirection: 'row',
