1444abc042792636953b8c2792e274081869b529
Matt Weidner <mweidner44@gmail.com>
2019-09-17 19:58:12 +0900

    Styling and component changes for comments

--- views/AnimatedEvent.tsx
+++ views/AnimatedEvent.tsx
@@ -146,7 +146,8 @@ export class AnimatedEvent extends Component<Props> {
           </Text>
           <View style={styles.image__container}>
             <Image
-              source={{ uri: this.props.currentEvent.cover_photo }}
+              // source={{ uri: this.props.currentEvent.cover_photo }}
+              source={require('../assets/quidditch.jpg')}
               style={styles.animated__photo}
             />
           </View>
@@ -171,7 +172,14 @@ export class AnimatedEvent extends Component<Props> {
             {editButton}
             {deleteButton}
           </View>
-          <Comments comments={this.props.currentEvent.comments} />
+          <View>
+            <View style={styles.comments_header}>
+              <Text style={styles.event__subtitle}>Comments</Text>
+            </View>
+            <View>
+              <Comments comments={this.props.currentEvent.comments} />
+            </View>
+          </View>
         </ScrollView>
       </Animated.View>
     )
@@ -211,6 +219,10 @@ const styles = StyleSheet.create({
     fontSize: 24,
     alignSelf: 'center',
     fontWeight: '800'
+  },
+  comments_header: {
+    marginTop: 20,
+    alignSelf: 'center',
   },
   event__subtitle: {
     marginTop: 20,
