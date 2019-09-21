8e7bfd632e1523e34b0ae864f91fb3939457c8fa
MarkDrastrup <mark.drastrup@email.com>
2019-09-21 18:06:10 +0900

    Changing proxmity to 30km instead of 10km

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -114,7 +114,7 @@ export const getUsers = (
       user =>
         !hiddenUsersObj[user.email] &&
         user.email !== currentUserEmail &&
-        distance(latitude, longitude, user.latitude, user.longitude) < 10
+        distance(latitude, longitude, user.latitude, user.longitude) < 30
     )
     dispatch({
       type: 'SET_ALL_USERS',
