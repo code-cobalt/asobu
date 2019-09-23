5f47aa82d183e72ddf316add04fac38830628468
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-23 17:57:27 +0900

    rename hangout_id to id

--- src/queries/users.ts
+++ src/queries/users.ts
@@ -128,7 +128,7 @@ export const loginQuery = gql`
         equipped_badges
       }
       ongoing_hangouts {
-        hangout_id
+        id
         participants {
           first_name
           email
